554aeec742da0f240507b1b35977919b5d0298ea
Knut Valen <knut.valen@icloud.com>
2024-06-03 16:10:30 +0200

    Add countdown card

---
+++ src/app/components/countdown/countdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './countdown.component.html',
+  styleUrl: './countdown.component.css',
+})
+export class CountdownComponent {}

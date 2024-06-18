c652d6422ddd4259a10f4f1c0ef051a1cea3f74b
Knut Valen <knut.valen@icloud.com>
2024-06-18 09:56:23 +0200

    Generate component

---
+++ src/app/components/next-launch/next-launch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-launch',
+  standalone: true,
+  imports: [],
+  templateUrl: './next-launch.component.html',
+  styleUrl: './next-launch.component.css'
+})
+export class NextLaunchComponent {
+
+}

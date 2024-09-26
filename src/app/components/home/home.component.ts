7c49c9d67025236052a4cd2ab023e192e4b4820d
Knut Valen <knut.valen@icloud.com>
2024-09-26 09:04:40 +0200

    Fix format

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { CountdownComponent } from '../countdown/countdown.component';
 import { PreviousLaunchesComponent } from '../previous-launches/previous-launches.component';
 import { NextLaunchComponent } from '../next-launch/next-launch.component';

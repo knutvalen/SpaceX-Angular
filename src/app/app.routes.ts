0913966dab93300d01733f348a3bd635517cc689
Knut Valen <knut.valen@icloud.com>
2024-10-25 13:50:30 +0200

    Add privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LaunchDetailsComponent } from './components/launch-details/launch-details.component';
 import { HomeComponent } from './components/home/home.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'launch', component: LaunchDetailsComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
